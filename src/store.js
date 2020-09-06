c07031d63d049742b2b19e52c5f8f46951333618
Adam Smith <coloradoasmith@gmail.com>
2020-09-06 17:10:48 -0600

    app running with route handler

---
+++ src/store.js
@@ -0,0 +1,11 @@
+const bookmarks = [
+  {
+    id: 1,
+    title: "The Library of Babel",
+    url: "https://libraryofbabel.info/",
+    description: "The Universal Library",
+    rating: 5,
+  },
+];
+
+module.exports = { bookmarks };
